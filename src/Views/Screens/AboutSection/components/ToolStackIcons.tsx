5ce3b7b6656e726caf4c403597eb54c5b2727b8a
Ammar Montaser <ammarmontaser@icloud.com>
2024-04-07 22:54:26 +0200

    edited tool stack icons color in light mode

--- src/Views/Screens/AboutSection/components/ToolStackIcons.tsx
+++ src/Views/Screens/AboutSection/components/ToolStackIcons.tsx
@@ -1,6 +1,6 @@
 function ToolStackIcons(props: { className: string }) {
   return (
-    <div className="flex flex-col justify-center items-center xl:size-[5rem]  size-[3rem]  dark:bg-[#282828] bg-slate-200 hover:shadow-lg rounded-xl">
+    <div className="flex flex-col justify-center items-center xl:size-[5rem]  size-[3rem]  dark:bg-[#282828] bg-white hover:shadow-lg rounded-xl">
       <span className={props.className}></span>
     </div>
   );
